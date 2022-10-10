0afb2c73fdaa1f30e9e22724629436ee5a8f02b8
Roméo GBONGBON <romeo.gbongbon@gmail.com>
2022-10-10 17:11:11 +0200

    Partie 7 - Encore plus de composition

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,11 +1,16 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
 
 
     public static Logger getLogger(String name) {
         Logger consoleLogger = new ConsoleLogger();
-        Logger fileLogger = new FileLogger("logging.txt");
-        return new CompositeLogger(consoleLogger, fileLogger);
+        Logger fileLogger = new FileLogger("logs.txt");
+        Predicate<String> condition = message -> message.contains("simulation");
+        Logger filteredLogger = new FilteredLogger(fileLogger, condition);
+        Logger contextualLogger = new ContextualLogger(name, filteredLogger);
+        return new CompositeLogger(contextualLogger, consoleLogger);
     }
 }
